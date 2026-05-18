b2e3f71f3770688a8e2b78c68d81255b3bf0a4c4
Abdelrahman Rizik <abdelrahman.rizik@gmail.com>
2026-05-18 19:58:21 +0300

    feat(direction-provider): add direction-provider

---
+++ packages/shardsui/src/lib/components/direction-provider/direction-provider.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+  import { DirectionContext, type TextDirection } from '$lib/internal/direction-context'
+
+  type Props = {
+    direction?: TextDirection
+    children?: Snippet
+  }
+
+  let { direction = 'ltr', children }: Props = $props()
+
+  DirectionContext.set({
+    get direction() {
+      return direction
+    }
+  })
+</script>
+
+{@render children?.()}
---
+++ packages/shardsui/src/lib/components/direction-provider/index.ts
@@ -0,0 +1,3 @@
+export { getDirection } from '$lib/internal/direction-context'
+export type { TextDirection } from '$lib/internal/direction-context'
+export { default as DirectionProvider } from './direction-provider.svelte'
