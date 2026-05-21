cd1333b5dd9a2e799b8a981d15476eeace7b6f4b
Abdelrahman Rizik <abdelrahman.rizik@gmail.com>
2026-05-21 16:00:17 +0300

    feat(separator): add separator

---
+++ packages/shardsui/src/lib/components/separator/separator.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { dataAttrs } from '$lib/internal/data-attrs'
+  import type { Orientation, PartProps } from '$lib/internal/types'
+
+  type Props = Omit<PartProps, 'children'> & {
+    orientation?: Orientation
+  }
+
+  let { as = 'div', ref = $bindable(null), orientation = 'horizontal', ...rest }: Props = $props()
+
+  const stateAttrs = $derived(dataAttrs({ orientation }))
+</script>
+
+<svelte:element
+  this={as}
+  bind:this={ref}
+  {...stateAttrs}
+  role="separator"
+  aria-orientation={orientation}
+  {...rest}
+/>
