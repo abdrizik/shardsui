c0e0b9e25c9780894b32cf0ba7588a20cd0b99f0
abdrizik <abdelrahman.rizik@gmail.com>
2026-08-13 18:19:03 +0300

    docs: add input page

---
+++ docs/src/lib/components/content/demos/input/hero/demo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Input } from '@shardsui/svelte/input'
+</script>
+
+<label class="flex flex-col items-start gap-1 text-sm font-semibold text-gray-900">
+  Title
+  <Input
+    placeholder="e.g. Design Systems"
+    class="h-8 w-64 rounded-md border border-gray-200 px-2 text-sm font-normal text-gray-900 focus:outline-2 focus:-outline-offset-1 focus:outline-gray-950 any-pointer-coarse:text-base"
+  />
+</label>
